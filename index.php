c8712e0d903685117b02dfcfa67c1a0c79b95b68
Watheq <watheq.alshowaiter@gmail.com>
2019-02-12 14:36:32 +0300

    done lesson1, but there is a trouble communicating to server

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 3;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'confident.ye@gmail.com';                 // SMTP username
+    $mail->Password = '774881466!';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    // $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;   // for tls                                 // TCP port to connect to
+    // $mail->Port = 465;
+
+    //Recipients
+    $mail->setFrom('confident.ye@gmail.com', 'Conf');
+    $mail->addAddress('confident.ye@gmail.com', 'Joe User');     // Add a recipient
+
+    $mail->addReplyTo('no-replay@example.com', 'No Replay');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>reset pass demo</title>
+</head>
+<body>
+	<!-- 	<p>this is for test</p> -->
+</body>
+</html>
