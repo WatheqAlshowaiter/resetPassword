5c3e583c1cd6e7d538446d295e8d536ad9c7bfd6
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2019-02-12 17:37:39 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php 
 
+// you can use reffered to your db settings. 
 $con = mysqli_connect("localhost", 'root', '', 'resetPassword'); 
 if (mysqli_connect_errno($con)) {
 	echo "connection failed :(" . mysqli_connect_errno($con);
